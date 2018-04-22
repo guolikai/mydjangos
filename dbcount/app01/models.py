bd75ea87075b1894b46385f974ac554b6246aa41
guolikai <glk73748196@sina.com>
2018-04-22 13:40:42 +0800

    Update models.py

--- dbcount/app01/models.py
+++ dbcount/app01/models.py
@@ -15,7 +15,9 @@ class  MysqlInfo(models.Model):
     mysql_passwd = models.CharField(verbose_name="密码",max_length=32)
     mysql_socket = models.CharField(verbose_name="Socket",max_length=32)
     mysql_remark = models.CharField(verbose_name="备注",max_length=32)
-    mysql_host = models.ForeignKey("HostInfo")
+    #在创建多对一的关系的,需要在Foreign的第二参数中加入on_delete=models.CASCADE主外关系键中，级联删除，
+    #也就是当删除主表的数据时候从表中的数据也随着一起删除
+    mysql_host = models.ForeignKey("HostInfo",on_delete=models.CASCADE)
     class Meta:
         verbose_name = 'MySQL信息'
     def __unicode__(self):
@@ -23,7 +25,7 @@ class  MysqlInfo(models.Model):
         return temp
 class DatabaseInfo(models.Model):
     database_name = models.CharField(verbose_name="数据库名",max_length=32)
-    database_mysql = models.ForeignKey("MysqlInfo")
+    database_mysql = models.ForeignKey("MysqlInfo",on_delete=models.CASCADE)
     class Meta:
         verbose_name = '数据库信息'
     def __unicode__(self):
@@ -32,7 +34,7 @@ class DatabaseInfo(models.Model):
 
 class DatabaseSize(models.Model):
     database_size = models.CharField(verbose_name="数据库大小[Mb]",max_length=32)
-    database_info = models.ForeignKey("DatabaseInfo")
+    database_info = models.ForeignKey("DatabaseInfo",on_delete=models.CASCADE)
 #    create_date = models.DateTimeField(verbose_name="创建日期",auto_now_add=True)
 #    update_date = models.DateTimeField(verbose_name="更新日期",auto_now=True)
     create_date = models.DateField(verbose_name="创建日期",auto_now_add=True)
@@ -45,9 +47,9 @@ class DatabaseSize(models.Model):
 class TableInfo(models.Model):
     table_name = models.CharField(verbose_name="数据库名",max_length=32)
 #    first_attr =  models.CharField(verbose_name="字段名",max_length=32)
-    table_database = models.ForeignKey("DatabaseInfo")
-    table_mysql = models.ForeignKey("MysqlInfo")
-    table_host = models.ForeignKey("HostInfo")
+    table_database = models.ForeignKey("DatabaseInfo",on_delete=models.CASCADE)
+    table_mysql = models.ForeignKey("MysqlInfo",on_delete=models.CASCADE)
+    table_host = models.ForeignKey("HostInfo",on_delete=models.CASCADE)
     class Meta:
         verbose_name = '表信息'
     def __unicode__(self):
@@ -55,7 +57,7 @@ class TableInfo(models.Model):
 
 class TableCount(models.Model):
     table_count = models.IntegerField(verbose_name="表记录数")
-    table_info = models.ForeignKey("TableInfo")
+    table_info = models.ForeignKey("TableInfo",on_delete=models.CASCADE)
 #    create_date = models.DateTimeField(verbose_name="创建日期",auto_now_add=True)
 #    update_date = models.DateTimeField(verbose_name="更新日期",auto_now=True)
     create_date = models.DateField(verbose_name="创建日期",auto_now_add=True)
